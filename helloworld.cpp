55e07573c213b5d6d0b57e79cdcb925f77d5398e
Jared E Jackson <jaredjxn15@gmail.com>
2018-05-21 22:20:03 -0400

    chore: added main func, and comments

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+// Comment syntax similar to javascript
+#include <iostream>
+
+// white-space lines have no effect on the program
+int main() {
+  std::cout << "Hello World!";
+}
+
+
+/*
+
+lines starting with a hash sign are directives read and
+interperated by a preprocesser, these special lines are interperated
+before the compilation of the program is run.
+
+int main()
+this line initiates the declartion of a function, 
+the function main() is a special function,
+it is the function that is called when the program is run,
+The execution of all c++ programs begins with the main function
+regardless of where the function is actually located in the code
+*/
