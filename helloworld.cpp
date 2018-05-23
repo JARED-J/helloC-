074560cda01124d5d5cf02b26d7026380c6ab594
Jared E Jackson <jaredjxn15@gmail.com>
2018-05-22 22:39:41 -0400

    added comments and syntax

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,10 +5,7 @@
 int main() {
   std::cout << "Hello World!";
 }
-
-
 /*
-
 lines starting with a hash sign are directives read and
 interperated by a preprocesser, these special lines are interperated
 before the compilation of the program is run.
@@ -19,4 +16,29 @@ the function main() is a special function,
 it is the function that is called when the program is run,
 The execution of all c++ programs begins with the main function
 regardless of where the function is actually located in the code
+
+for loop syntax 
+
+for (init; condition; increment) {
+    statement(s);
+}
 */
+
+using namespace std;
+
+int loop() {
+    // for loop execution
+    for (int a = 10; a < 20; a = a + 1) {
+        cout << "value of a: " << a << endl;
+    }
+    return 0;
+}
+// functions can be written inline,
+// whitespace doesn't effect how the function gets ran
+// so the next functions will do the same thing
+int oneLine () { std::cout << " Hello World! "; std::cout << " I'm a C++ program "; }
+
+int multipleLine() {
+    cout << "Hello World";
+    cout << "I'm a c++ program";
+}
